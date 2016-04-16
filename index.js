a3fdc878da6817f8804f294c57d52eb708b47939
Amin <mohamed.amin.088@gmail.com>
2016-04-16 23:03:07 +0400

    forgot to export

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
- var merge = require('src/merge.js');
+'use strict';
+
+module.exports = function merge(obj, base, secondary) {
+	obj = obj || {};
+    base = base || (function(){throw new Error('Should define base Object');}());
+    secondary = secondary || (function() {throw new Error('Should define secondary Object');}());
+    for (var key in base) {
+        if (secondary[key]) {
+            obj[key] = Object.assign({}, base[key], secondary[key]);
+        } else {
+            obj[key] = base[key];
+        }
+    }
+    return obj;
+};
