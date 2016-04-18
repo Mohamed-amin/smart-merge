f656279d1224104763af29cdfed735a094da6e7b
Amin <mohamed.amin.088@gmail.com>
2016-04-18 23:42:46 +0400

    Recursive merger with tests

--- index_spec.js
+++ index_spec.js
@@ -36,14 +36,34 @@ if (typeof Object.assign != 'function') {
 }
 
 describe("Merge", function(){
-	var a, b, c, d, e;
+	var a, b, c, d, e, complix1, complix2;
 	beforeEach(function(){
 		a = {a : 1};
 		b = {b : 1};
 		c = {c : 1};
 		d = {d : 1};
 		e = {e : 1};
-	})
+		complix1 = {
+			a:{
+				a:{
+					1:'1'
+				},
+				b:{
+					1:'1'
+				}
+			}
+		};
+		complix2 = {
+			a:{
+				a:{
+					2:'2'
+				},
+				b:{
+					2:'2'
+				}
+			}
+		};
+	});
 	it('Merge object 1 properties to Object 2', function(){
 		var baseStyles = {
 		    header: {
@@ -63,12 +83,28 @@ describe("Merge", function(){
 	});
 
 	it('Should accept 1 Parameter', function(){
-		// need to be covered
+		expect(merge(a,b)).toEqual({a:1,b:1});
 	});
 
 	it('Should accept any number of Parameters', function(){
-		// need to be covered
-	})
+		expect(merge(a,b,c,d,e)).toEqual({a:1,b:1,c:1,d:1,e:1});
+	});
+
+	it('Should accept any depth complixty', function(){
+		
+		expect(merge(complix1, complix2)).toEqual({
+			a:{
+				a:{
+					1:'1',
+					2:'2'
+				},
+				b:{
+					1:'1',
+					2:'2'
+				}
+			}
+		});
+	});
 
 });
 
